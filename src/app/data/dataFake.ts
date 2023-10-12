88cbaf20d42bca37d1b3247d7ffe1b7f43938c5c
Juuhcso <juuhtambasco@gmail.com>
2023-10-12 07:34:06 -0300

    finalizado

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -10,5 +10,18 @@ export const dataFake = [
     "title": "Diferentes tipos de feitiços em Jujutsu Kaisen",
     "description": "Além de personagens super carismáticos e um mundo fascinante, Jujutsu Kaisen também apresentou poderes e técnicas dignos dos melhores animes – seja do lado dos heróis ou vilões. No universo de Jujutsu, pessoas com energia amaldiçoada, com a chance de se tornar feiticeiros ou usuários de maldições, podem carregar dois tipos de técnicas principais: Técnicas Hereditárias e Técnicas Inatas. É a diferença entre o que é transmitido por uma linhagem sanguínea ou aquilo que é próprio de um indivíduo.",
     "photoCover":"https://kanto.legiaodosherois.com.br/fnoop-cfill/wp-content/uploads/2020/10/legiao_CPnE837M0xvN.png.webp"
+  },
+  {
+    "id":"3",
+  "title": "Um review de um novo filme ou anime de Jujutsu Kaisen.",
+  "description": "O jovem Yuta Okkotsu ganha o controle de um espírito extremamente poderoso, então um grupo de feiticeiros o matriculam na Tokyo Prefectural Jujutsu High School, para ajudá-lo a controlar esse poder e também para ficar de olho nele.",
+  "photoCover":"https://poltronanerd.com.br/wp-content/uploads/2022/02/poltrona-jujutsu-kaisen-0-data-de-estreia.jpg"
+
+  },
+
+  { "ide":"4",
+  "title": "O futuro de Jujutsu Kaisen.",
+  "description":"",
+  "photoCover": "https://pbs.twimg.com/media/FZ0MvSeWIAAvRWE.jpg:large"
   }
 ]
